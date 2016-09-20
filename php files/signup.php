020b3c6d5d2dc6765eb618d8b1df7d1223200fef
Rasha El-Manzalawy <rashaelmanzalawy@Rashas-MacBook-Pro.local>
2016-09-20 16:01:08 +0200

    added textfield in aignup.php

--- php files/signup.php
+++ php files/signup.php
@@ -5,6 +5,7 @@
 	<form action="signedup.php" method="post">
 	Username: <input type="text" name="username"><br>
 	Password: <input type="text" name="password"><br>
+	Home address: <input type="text" name="home address"><br>
 	<input type="submit">
 	</form>
 </body>
