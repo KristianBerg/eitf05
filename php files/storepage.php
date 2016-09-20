c8ec9054d60742ed135da630331eb6dfd311b50e
Kristian Berg <kristianberg.jobb@gmail.com>
2016-09-20 16:27:34 +0200

    Added store page mockup

---
+++ php files/storepage.php
@@ -0,0 +1,17 @@
+<html>
+<head></head>
+<body>
+	<h2> Welcome to the store! (TM) </h2>
+	<h4> Available items: </h4>
+	
+	<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Maple_Leaf.svg/2000px-Maple_Leaf.svg.png" alt="dank leaf" style="width:200; height:200;"> <br>
+	<form action = "storepage.php" method = "post">
+	Number of cannadis: <input type="text" name="noItem1">
+	<input type = "submit"><br>
+	<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Linear_subspaces_with_shading.svg/2000px-Linear_subspaces_with_shading.svg.png" alt="trippin" style="width:200; height: 200"> <br>
+	<form action = "storepage.php" method = "post">
+	Number of lines: <input type="text" name="noItem2">
+	<input type = "submit"> <br> <br>
+	<iframe src="//giphy.com/embed/xTiTnHXbRoaZ1B1Mo8" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+</body>
+</html>
