3d2e115ef24d9086f961f4517362a38c2b0a7e0a
Rasha El-Manzalawy <rashaelmanzalawy@Rashas-MacBook-Pro.local>
2016-09-21 13:56:24 +0200

    form in storepage redirected to shoppingcart

--- php files/shoppingcart.php
+++ php files/shoppingcart.php
@@ -1,1 +1,12 @@
 
+
+<html>
+<head></head>
+<body>
+
+<h2> This is your shopping cart! </h2>
+<h3> Items in shopping cart </h3>
+ 
+
+</body>
+</html>
--- php files/signup.php
+++ php files/signup.php
@@ -4,7 +4,7 @@
 	<h3> sign up </h3>
 	<form action="signedup.php" method="post">
 	Username: <input type="text" name="username"><br>
-	Password: <input type="text" name="password"><br>
+	Password: <input type="password" name="password"><br>
 	Home address: <input type="text" name="home address"><br>
 	<input type="submit">
 	</form>
