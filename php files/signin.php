50b6ea407a96796afd902da1db410c5b5c8b58a5
HerrAndersson95 <dic14jan@student.lu.se>
2016-09-21 14:31:55 +0200

    ändrat text field till password (OCD)
    ändrat password field till type password

--- php files/signin.php
+++ php files/signin.php
@@ -5,7 +5,7 @@
 	<h3> Login </h3>
 	<form action="login.php" method="post">
 	  Username: <input type="text" size="20" name="username"><br>
-	  Password: <input type="text" size="20" name="password"><br>
+	  Password: <input type="password" size="20" name="password"><br>
 	  <input type="submit" value="login">
 	</form>
 	<form action="signup.php" method="post">
